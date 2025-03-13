9cd3951c1458547580f361e4f4a96e923a2326d0
van3ll0pe <van3ll0pe.chell@gmail.com>
2025-03-13 09:06:27 +0100

    Add pwm controller

---
+++ pwm.vhd
@@ -0,0 +1,57 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity PWM is
+    generic(
+        DUTY_SIZE : natural := 8;
+        MAX_CPT : natural := 1E6
+    );
+    port(
+        clk : in std_logic;
+        rst : in std_logic;
+        duty : in std_logic_vector(DUTY_SIZE-1 downto 0);
+        dout : out std_logic
+    );
+end PWM;
+
+architecture behavior of PWM is
+    signal duty_value : natural range 0 to 100 := 0;
+    signal nbr_count : natural range 0 to MAX_CPT := 0;
+begin
+
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            duty_value <= (to_integer(unsigned(duty)) * 100)  / (2**DUTY_SIZE - 1);
+        end if;
+    end process;
+
+    process(clk)
+    begin
+        nbr_count <= (duty_value * MAX_CPT) / 100;
+    end process;
+
+    process(clk, rst)
+        variable counter : natural range 0 to (MAX_CPT - 1) := 0;
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                counter := 0;
+                dout <= '0';
+            else
+                dout <= '0';
+                
+                if counter = MAX_CPT - 1 then
+                    counter := 0;
+                else
+                    counter := counter + 1;
+                end if;
+
+                if counter < nbr_count then
+                    dout <= '1';
+                end if;
+            end if;
+        end if;
+    end process;
+end behavior;
---
+++ test_pwm.vhd
@@ -0,0 +1,116 @@
+---------------------------------------
+-- Pulse generator test architecture
+--
+-- For GHDL users:
+-- ghdl -a --ieee=synopsys -fexplicit pulse_gen.vhd test_pulse_gen.vhd
+-- ghdl -e --ieee=synopsys -fexplicit test_pulse_gen
+-- ghdl -r --ieee=synopsys -fexplicit test_pulse_gen --wave=test_pulse_gen.ghw
+-- gtkwave test_pulse_gen.ghw
+--
+-- F.Thiebolt
+---------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+-- component definition
+entity test_pwm is
+end test_pwm;
+
+-- architecture definition
+architecture behaviour of test_pwm is
+
+    -- constant defintions
+	constant TIMEOUT 	: time := 25000 ms; -- simulation timeout
+    constant clkpulse   : Time := 500 ns; -- 1/2 periode horloge
+
+    -- types/subtypes definitions
+
+    -- signal definitions
+    signal E_CLK  : std_logic;
+    signal E_RST        : std_logic; -- active low
+    signal E_DUTY : std_logic_vector(7 downto 0) := "11000000";
+    signal E_DOUT : std_logic := '0';
+
+begin
+
+--------------------------
+-- definition de l'horloge
+P_E_CLK: process
+begin
+	E_CLK <= '1';
+	wait for clkpulse;
+	E_CLK <= '0';
+	wait for clkpulse;
+end process P_E_CLK;
+
+-----------------------------------------
+-- definition du timeout de la simulation
+P_TIMEOUT: process
+begin
+	wait for TIMEOUT;
+	assert FALSE report "SIMULATION TIMEOUT!!!" severity FAILURE;
+end process P_TIMEOUT;
+
+--------------------------------------------------
+-- instantiation et mapping du composant registres
+pgen0 : entity work.pwm(behavior)
+			generic map (8, 1E6)
+			port map (clk => E_CLK,
+                      rst => E_RST,
+                        duty=> E_DUTY,
+                        dout => E_DOUT);
+
+-----------------------------
+-- Test process
+P_TEST: process
+begin
+
+	-- initialisations
+	E_RST <= '0';
+    --E_CLK <= '0'; -- DON'T DO THAT ... guess why ???
+
+	-- sequence RESET
+	--E_RST <= '0';
+	--wait for clkpulse*3;
+	--E_RST <= '1';
+	--wait for clkpulse/2;
+
+    -- wait for pulse output
+	wait until (E_DOUT='1');
+
+    -- wait for pulse output
+	wait until (E_DOUT='1');
+
+    wait until (E_DOUT='1');
+    wait until (E_DOUT='1');
+
+    E_DUTY<= "10000000";
+
+    wait until (E_DOUT='1');
+
+    wait until (E_DOUT='1');
+    wait until (E_DOUT='1');
+    wait until (E_DOUT='1');
+
+    E_DUTY <= "01000000";
+
+    wait until (E_DOUT='1');
+    wait until (E_DOUT='1');
+    wait until (E_DOUT='1');
+
+    E_DUTY <= "11111111";
+
+    wait until (E_DOUT='1');
+    wait until (E_DOUT='1');
+    wait until (E_DOUT='1');
+	-- ADD NEW SEQUENCE HERE
+
+	-- LATEST COMMAND (NE PAS ENLEVER !!!)
+	wait until (E_CLK='0'); wait for clkpulse*3;
+	assert FALSE report "FIN DE SIMULATION" severity FAILURE;
+
+end process P_TEST;
+
+end behaviour;
